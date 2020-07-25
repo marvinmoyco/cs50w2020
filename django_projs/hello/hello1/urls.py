49f154bab853420757064cb7a66cef7d032990e7
user123 <marvin.moyco@gmail.com>
2020-07-25 19:12:44 +0800

    Added new files of lec3

--- django_projs/hello/hello1/urls.py
+++ django_projs/hello/hello1/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path 
 urlpatterns=[
     path("",views.index,name="index"),
+    path("<str:str1>",views.greet, name="greet"),
     path("marvin",views.marvin,name="marvin")
 ]
--- django_projs/hello/hello1/views.py
+++ django_projs/hello/hello1/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request,"hello1/index.html")
 
 def marvin(request):
     return HttpResponse("Hello, Marvin")
+
+def greet(request,str1):
+    return render(request,"hello1/greet.html",{
+        "name": str1.capitalize()
+    })
