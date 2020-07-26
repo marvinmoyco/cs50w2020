31669ac5cc8125b35dde704a9da4095951eb433d
user123 <marvin.moyco@gmail.com>
2020-07-26 21:16:37 +0800

    Added pages and Index page almost complete!

--- Project1-Wiki/wiki/encyclopedia/urls.py
+++ Project1-Wiki/wiki/encyclopedia/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+app_name = "wiki"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry_title>",views.entry,name="entry")
 ]
--- Project1-Wiki/wiki/encyclopedia/views.py
+++ Project1-Wiki/wiki/encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request,entry_title):
+    return render(request,"encyclopedia/entry.html",{
+        "entry": util.get_entry(entry_title),
+        "entry_str": entry_title.capitalize()
+    })
