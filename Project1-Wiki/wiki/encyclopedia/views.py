35510f9e7e84da3ba8979ec177d81e0f6b28d1d6
user123 <marvin.moyco@gmail.com>
2020-07-27 03:47:38 +0800

    Added features to entry page

--- Project1-Wiki/wiki/encyclopedia/views.py
+++ Project1-Wiki/wiki/encyclopedia/views.py
@@ -9,7 +9,38 @@ def index(request):
     })
 
 def entry(request,entry_title):
-    return render(request,"encyclopedia/entry.html",{
-        "entry": util.get_entry(entry_title),
-        "entry_str": entry_title.capitalize()
-    })
+    str1 = entry_title.lower()
+    for str2 in util.list_entries():
+        str2 = str2.lower()
+        check_str = str1.find(str2,0,len(str2))
+        if check_str == 0:
+            break
+
+    if check_str == -1:
+        return render(request,"encyclopedia/entry_error.html",{
+            "entry_str": entry_title.capitalize()
+        })
+    elif check_str == 0:
+        if util.get_entry(entry_title) is None:
+            return render(request,"encyclopedia/entry_error.html",{
+                "entry_str": entry_title.capitalize()
+            })
+        return render(request,"encyclopedia/entry.html",{
+            "entry": util.get_entry(entry_title),
+            "entry_str": entry_title.capitalize()
+        })
+
+#def add_entry(request):
+#    return render(request, "encyclopedia/add_entry.html",{
+#
+#    })
+
+#def edit_entry(request):
+#    return render(request, "encyclopedia/add_entry.html",{
+#
+#    })
+
+#def random_entry(request):
+#    return render(request, "encyclopedia/add_entry.html",{
+#
+#    })
