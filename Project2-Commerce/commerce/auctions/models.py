7cd490b15a21b1c4809c5b7612f4a8da271b007a
marvinmoyco <marvin.moyco@gmail.com>
2020-09-12 20:09:46 +0800

    Added watchlist button and bid button on listing page

--- Project2-Commerce/commerce/auctions/models.py
+++ Project2-Commerce/commerce/auctions/models.py
@@ -3,9 +3,14 @@ from django.db import models
 from django.utils import timezone
 
 class User(AbstractUser):
+    pass
+
+class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
     bid = models.PositiveIntegerField(default=0)
-    listing = models.ManyToManyField('Listing',blank=True,related_name="listing")
+    listing = models.ManyToManyField('Listing',blank=True,related_name="user_listing")
     date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('User',blank=True,on_delete=models.CASCADE,related_name="current_bidder",default=None)
 
  
 class Listing(models.Model):
@@ -15,12 +20,13 @@ class Listing(models.Model):
     url = models.URLField()
     date_created = models.DateTimeField(auto_now_add=True)
     categories = models.CharField(max_length=256,blank=True)
-    bid_winner = models.ForeignKey('User',blank=True,on_delete=models.CASCADE,related_name="winner",default=None)
+    bid_winner = models.ForeignKey('User',null=True,blank=True,on_delete=models.CASCADE,related_name="winner",default=None)
     creator = models.ForeignKey('User',on_delete=models.CASCADE,related_name="creator_listing",default=None)
     closed = models.IntegerField(default = 0)
     
 
 class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
     username = models.ForeignKey('User',on_delete=models.CASCADE,related_name = "user_comment",default=None)
     comment = models.TextField(default="")
     comment_date = models.DateTimeField(auto_now=True)
@@ -28,8 +34,9 @@ class Comment(models.Model):
     
 class Categories(models.Model):
     id = models.AutoField(primary_key=True)
-    categories = models.TextField(default="")
+    categories = models.TextField(default="",unique=True,blank=True,null=True)
 
 class Watchlist(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ManyToManyField('User', blank=True, related_name="user_watchlist",default=None)
     listing = models.ManyToManyField('Listing',blank=True,related_name="watchlist",default=None)
--- Project2-Commerce/commerce/auctions/views.py
+++ Project2-Commerce/commerce/auctions/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django import forms
 from .models import *
+import datetime
 class New_Listing(forms.Form):
     title = forms.CharField()
     description = forms.CharField(widget=forms.Textarea)
@@ -13,12 +14,17 @@ class New_Listing(forms.Form):
     img_url = forms.URLField()
     categories = forms.CharField()
 
+class New_Bid(forms.Form):
+    new_bid = forms.IntegerField(min_value=0)
 
+class New_Comment(forms.Form):
+    comment = forms.CharField(widget=forms.Textarea)
 
 def index(request):
     
     return render(request, "auctions/index.html",{
         "active_listings": Listing.objects.all(),
+        "active_bids": Bid.objects.all(),
         "index_active":True
     })
 
@@ -30,19 +36,20 @@ def add_listing(request):
         if new_listing.is_valid():
             saved_categories = new_listing.cleaned_data["categories"]
             categories = saved_categories.split(",")
-            
-            
 
             save_listing = Listing(title=new_listing.cleaned_data["title"],description=new_listing.cleaned_data["description"],url=new_listing.cleaned_data["img_url"],creator=request.user,categories=saved_categories)
-            
-            request.user.bid = new_listing.cleaned_data["initial_bid"]
-            request.user.listing.set(save_listing)
-           
-            
             save_listing.save()
+            bid = Bid(bid = int(new_listing.cleaned_data["initial_bid"]),user=request.user)
+            # save() is needed before adding a many to many relationship field
+            bid.save() 
+            bid.listing.add(save_listing)
+            bid.save()
             for x in categories:
-                each_categ = Categories(categories=x)
-                each_categ.save()
+                try:
+                    each_categ = Categories(categories=x)
+                    each_categ.save()
+                except IntegrityError:
+                    break
             return HttpResponseRedirect(reverse('auction:index'))
     else:
         return render(request,"auctions/create.html",{
@@ -107,8 +114,80 @@ def register(request):
 
 def listing(request,listing_name):
     current_listing = Listing.objects.get(title = listing_name)
+    current_bid = Bid.objects.get(listing=current_listing)
+    comment = Comment.objects.filter(listing=current_listing)
+    new_comment1 = New_Comment()
+    new_bid1 = New_Bid()
+    if request.method == "POST":
+        # When the form submitted is a comment form
+        if 'comment_add' in request.POST:
+            new_comment = New_Comment(request.POST)
+            if new_comment.is_valid():
+                text_comment = new_comment.cleaned_data["comment"]
+                save_comment = Comment(username=request.user,comment=text_comment)
+                save_comment.save()
+                save_comment.listing.add(current_listing)
 
+        # When the form submitted is a update bid form
+        elif 'bid_add' in request.POST:
+            new_bid = New_Bid(request.POST)
+            if new_bid.is_valid():
+                if new_bid.cleaned_data["new_bid"] > current_bid.bid:
+                    Bid.objects.filter(listing=current_listing).update(bid=int(new_bid.cleaned_data["new_bid"]),user=request.user,date=datetime.datetime.now())
+                    return render(request, "auctions/listing.html",{
+                        "current_listing":current_listing,
+                        "current_bid": current_bid,
+                        "comments": comment,
+                        "comment_form": new_comment1,
+                        "bid_form": new_bid,
+                        "message": "The bid is successfully updated!"
+
+                    })
+                else:
+                    return render(request, "auctions/listing.html",{
+                        "current_listing":current_listing,
+                        "current_bid": current_bid,
+                        "comments": comment,
+                        "comment_form": new_comment1,
+                        "bid_form": new_bid,
+                        "message": "The bid should be higher than the current bid."
+
+                    })
+        # When the listing is added to the user's Watchlist
+        elif 'watchlist_add' in request.POST:
+            list_watchlist = Watchlist.objects.filter(user=request.user)
+            try:
+                list_watchlist.get(listing=current_listing)
+            except Watchlist.DoesNotExist:
+                new_watchlist = Watchlist()
+                new_watchlist.save()
+                new_watchlist.user.add(request.user)
+                new_watchlist.listing.add(current_listing)
+                return render(request, "auctions/listing.html",{
+                "current_listing":current_listing,
+                "current_bid": current_bid,
+                "comments": comment,
+                "comment_form": new_comment1,
+                "bid_form": new_bid1,
+                "message": "The listing is successfully added to your watchlist"
+                })
+            return render(request, "auctions/listing.html",{
+                "current_listing":current_listing,
+                "current_bid": current_bid,
+                "comments": comment,
+                "comment_form": new_comment1,
+                "bid_form": new_bid1,
+                "message": "The listing is already in the watchlist"
+            })   
+            
+          
 
     return render(request, "auctions/listing.html",{
-        "current_listing":current_listing
+        "current_listing":current_listing,
+        "current_bid": current_bid,
+        "comments": comment,
+        "comment_form": new_comment1,
+        "bid_form": new_bid1,
+        "message": None
+
     })
