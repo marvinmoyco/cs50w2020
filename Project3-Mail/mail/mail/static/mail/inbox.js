88a5aface0e959cdc19b450fd81ab7e1d3601336
marvinmoyco <marvin.moyco@gmail.com>
2021-08-08 08:36:43 +0800

    CSuccessfully created inbox view (not complete)

--- Project3-Mail/mail/mail/static/mail/inbox.js
+++ Project3-Mail/mail/mail/static/mail/inbox.js
@@ -20,6 +20,26 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  //Code when email is sent
+  document.querySelector('form').onsubmit = () => {
+    alert("Entering onsubmit")
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+        alert(result)
+    });
+    
+  };
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +50,40 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch('/emails/'+mailbox)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+      console.log(emails[0].subject)
+      // ... do something else with emails ...
+      document.querySelector('#emails-view').innerHTML += `<div class="list-group">`;
+      for (let i=0;i<emails.length;i++)
+      {
+        document.querySelector('#emails-view').innerHTML += `<a href="#" class="list-group-item list-group-item-action">
+                                                                <div class="d-flex w-100 justify-content-between">
+                                                                  <h5 class="mb-1">${emails[i].subject}</h5>
+                                                                  <small class="text-muted">${emails[i].timestamp}</small>
+                                                                </div>
+                                                                <p class="mb-1">${emails[i].recipients}</p>
+                                                                <small class="text-muted">${emails[i].body}</small>
+                                                              </a>
+                                                                `;
+        
+        
+        
+        
+        //document.querySelector('#emails-view').innerHTML += `<div class="d-flex w-100 justify-content-between">`;
+        //document.querySelector('#emails-view').innerHTML += `<h5 class="mb-1">${emails[i].subject}</h5>`;
+        //document.querySelector('#emails-view').innerHTML += `<small class="text-muted">${emails[i].timestamp}</small>`;
+        //document.querySelector('#emails-view').innerHTML += `</div>`;
+        //document.querySelector('#emails-view').innerHTML += `<p class="mb-1">${emails[i].recipients}</p>`;
+        //document.querySelector('#emails-view').innerHTML += `<small class="text-muted">${emails[i].body}</small>`;
+        //document.querySelector('#emails-view').innerHTML += `</a>`;
+      }
+      //document.querySelector('#emails-view').innerHTML += `</div">`;
+  });
+
+
 }
